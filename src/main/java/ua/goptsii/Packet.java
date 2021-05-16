05ed7f57ababebfdec9de2102651a5dedf377151
Kateryna Kolokhina <kateryna_kolokhina@epam.com>
2021-05-16 17:46:52 +0300

    edit Packet class, add TODO

--- src/main/java/ua/goptsii/Packet.java
+++ src/main/java/ua/goptsii/Packet.java
@@ -2,6 +2,7 @@ package ua.goptsii;
 
 import com.github.snksoft.crc.CRC;
 import com.google.common.primitives.UnsignedLong;
+
 import java.nio.ByteBuffer;
 
 public class Packet {
@@ -13,7 +14,9 @@ public class Packet {
     private Message bMsq;
     private Short wCrc16_2;
 
-    public Packet(byte bSrc, UnsignedLong bPktId, Integer wLen, short wCrc16_1, Message bMsq, short wCrc16_2){
+    public static final int LENGTH_FIRST_PART_PACKAGE = Byte.BYTES + Byte.BYTES + Long.BYTES + Integer.BYTES;
+
+    public Packet(byte bSrc, UnsignedLong bPktId, Integer wLen, short wCrc16_1, Message bMsq, short wCrc16_2) {
         this.bSrc = bSrc;
         this.bPktId = bPktId;
         this.wLen = wLen;
@@ -22,60 +25,78 @@ public class Packet {
         this.wCrc16_2 = wCrc16_2;
     }
 
-    public Packet(byte[] encodedPacket) throws Exception{
-    ByteBuffer buffer = ByteBuffer.wrap (encodedPacket);
-    Byte bMagicCheck = buffer.get();
-    if (!bMagicCheck.equals(this.bMagic)){
-        throw new Exception("Wrong bMagic!");
-    }
-    bSrc = buffer.get();
-    long pktId = buffer.getLong();
-    bPktId = UnsignedLong.fromLongBits(pktId);
-    wLen = buffer.getInt();
-    wCrc16_1= buffer.getShort();
-        if (packetFirstLength()!=(wCrc16_1)){
+    public Packet(byte[] encodedPacket) throws Exception {
+        ByteBuffer buffer = ByteBuffer.wrap(encodedPacket);
+        Byte bMagicCheck = buffer.get();
+
+        if (!bMagicCheck.equals(this.bMagic)) {
+            throw new Exception("Wrong bMagic!");
+        }
+
+        bSrc = buffer.get();
+        long pktId = buffer.getLong();
+        bPktId = UnsignedLong.fromLongBits(pktId);
+        wLen = buffer.getInt();
+
+        //TODO CALCULATE wCrc16_1Calculated AS WE DO IT BEFORE
+        Short wCrc16_1Calculated = 0;
+
+        wCrc16_1 = buffer.getShort();
+
+        if (!wCrc16_1Calculated.equals(wCrc16_1)) {
             throw new Exception("Wrong wCrc16_1!");
         }
+
         ////////////////////////////
-    int cType=buffer.getInt();
-    int bUserId=buffer.getInt();
-    byte[] messageText= new byte[wLen];
-    buffer.get(messageText);
-    bMsq = new Message(cType,bUserId, new String (messageText));
-    bMsq.decode();
-    wCrc16_2= buffer.getShort();
-        if (packetSecondLength()!=(wCrc16_2)){
+
+        int cType = buffer.getInt();
+        int bUserId = buffer.getInt();
+        byte[] messageText = new byte[wLen];
+        buffer.get(messageText);
+        bMsq = new Message(cType, bUserId, new String(messageText));
+
+        bMsq.decode();
+
+        //TODO CALCULATE wCrc16_2Calculated AS WE DO IT BEFORE
+        Short wCrc16_2Calculated = 0;
+
+        wCrc16_2 = buffer.getShort();
+
+        if (!wCrc16_2Calculated.equals(wCrc16_2)) {
             throw new Exception("Wrong wCrc16_2!");
         }
     }
 
-
-    public int packetFirstLength(){
-        return Byte.BYTES+Byte.BYTES+Long.BYTES+Integer.BYTES;
-    }
-
-    public int packetSecondLength(){
+    public int packetSecondLength() {
         return bMsq.getMessageBytesLength();
     }
-    public byte[] toPacket() throws Exception{
-    Message message = bMsq;
+
+    public byte[] toPacket() {
+        Message message = bMsq;
         message.encode();
-    byte[] packetPartFirst = ByteBuffer.allocate(packetFirstLength())
-            .put(bMagic)
-            .put(bSrc)
-            .putLong(bPktId.longValue())
-            .putInt(wLen)
-            .array();
+
+        byte[] packetPartFirst = ByteBuffer.allocate(LENGTH_FIRST_PART_PACKAGE)
+                .put(bMagic)
+                .put(bSrc)
+                .putLong(bPktId.longValue())
+                .putInt(wLen)
+                .array();
+
+        //CRC of first part of message
+        wCrc16_1 = (short) CRC.calculateCRC(CRC.Parameters.CRC16, packetPartFirst);
 
         byte[] packetPartSecond = ByteBuffer.allocate(packetSecondLength())
                 .put(bMsq.getMessageForPacket())
                 .array();
 
-        wCrc16_1= (short)CRC.calculateCRC(CRC.Parameters.CRC16, packetPartFirst);
-        wCrc16_2=(short)CRC.calculateCRC(CRC.Parameters.CRC16, packetPartSecond);
+        //CRC of message
+        wCrc16_2 = (short) CRC.calculateCRC(CRC.Parameters.CRC16, packetPartSecond);
 
-        int packetLength = packetPartFirst.length+packetPartSecond.length+wCrc16_1+wCrc16_2;
-        return ByteBuffer.allocate(packetLength).put(packetPartFirst)
+
+        int packetLength = LENGTH_FIRST_PART_PACKAGE + Short.BYTES + packetSecondLength() + Short.BYTES;
+
+        return ByteBuffer.allocate(packetLength)
+                .put(packetPartFirst)
                 .putShort(wCrc16_1)
                 .put(packetPartSecond)
                 .putShort(wCrc16_2)
--- src/main/java/ua/goptsii/Test.java
+++ src/main/java/ua/goptsii/Test.java
@@ -1,8 +1,11 @@
 package ua.goptsii;
 
+import com.github.snksoft.crc.CRC;
+
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -12,6 +15,15 @@ public class Test {
     public static void main(String[] args) {
 
         Message message = new Message(12, 12, "test");
+
+        byte[] packetPartSecond = ByteBuffer.allocate(message.getMessageBytesLength() )
+                .put(message.getMessageForPacket())
+                .array();
+
+//        //CRC of message
+//        Short wCrc16_2=(short) CRC.calculateCRC(CRC.Parameters.CRC16, packetPartSecond);
+//
+//        System.out.println(wCrc16_2+" "+wCrc16_2.BYTES+" "+ Short.BYTES);
 
         System.out.println(message.getMessage());
         System.out.println(message.getMessage());
